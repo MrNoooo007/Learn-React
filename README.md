313730a545a11493666fcc5d0cf8e4ca8cbd1112
DatTran <khiemtran21012000@gmail.com>
2023-04-24 23:15:25 +0700

    add code

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Learn-React
+
+## Arrow function trong Javascript ES6
+- arrow function khong co context
+- khong co constructor
---
+++ main.js
@@ -0,0 +1,9 @@
+let logger = (log) => {
+    console.log(log)
+}
+
+let sum = (a,b) => a + b;
+
+let returnObj = (a,b) => ({ a:a, b:b});
+
+console.log(sum(1,2));
