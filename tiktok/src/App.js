65ca924c40148ae69ee81243d4943f0b4fec310c
DatTran <khiemtran21012000@gmail.com>
2023-05-06 23:46:35 +0700

    add todo

--- tiktok/src/App.js
+++ tiktok/src/App.js
@@ -1,75 +1,28 @@
 import './App.css';
 import { useState } from 'react';
 
-const courses = [
-  {
-    id: 1,
-    name: "HTML"
-  },
-  {
-    id: 2,
-    name: "CSS"
-  },
-  {
-    id: 3,
-    name: "JS"
-  }
-]
-
 
 function App() {
-  const [name, setName] = useState('');
-  const [checked, setChecked] = useState([]);
+  const [jobs, setJobs] = useState([]);
+  const [job, setJob] = useState('');
 
-  function handleRadio() {
-    console.log(checked);
-  }
 
-  function handleCheck(id) {
-    setChecked(pre => {
-      const isChecked = checked.includes(id);
-      if(isChecked) {
-        return checked.filter(check => check != id);
-      }
-      else {
-        return [...pre,id]
-      }
-    })
+  const handleAdd = () => {
+    setJobs(pre => [...pre, job])
+    setJob('');
   }
-  console.log(checked);
 
   return (
     <div className="App">
-
-      {/* Two-ay binding for input  */}
-      <input
-        value={name}
-        onChange={(e) => setName(e.target.value)}
+      <input 
+        value={job}
+        onChange={(e) => setJob(e.target.value)}
       />
-      <h1>{name}</h1>
+      <button onClick={handleAdd}>Add</button>
 
-      {/* Two-way binding for radio */}
-      {/* {courses.map(course =>
-        <div key={course.id}>
-          <input
-            type='radio'
-            checked={course.id === checked}
-            onChange={(e) => setChecked(course.id)}
-          /> {course.name}
-        </div>
-      )} */}
-
-      {courses.map(course =>
-        <div key={course.id}>
-          <input
-            type='checkbox'
-            checked={checked.includes(course.id)}
-            onChange={() => handleCheck(course.id)}
-          /> {course.name}
-        </div>
-      )}
-
-      <button>Sumit</button>
+      <ul>
+        {jobs.map((job,index) => <li key={index}>{job}</li>)}
+      </ul>
     </div>
   );
 }
