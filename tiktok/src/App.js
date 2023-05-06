0586b264b3b9c50b86839c2365b79e5777cdec7a
DatTran <khiemtran21012000@gmail.com>
2023-05-06 23:17:18 +0700

    add code

--- tiktok/src/App.js
+++ tiktok/src/App.js
@@ -19,33 +19,57 @@ const courses = [
 
 function App() {
   const [name, setName] = useState('');
-  const [checked, setChecked] = useState(1);
+  const [checked, setChecked] = useState([]);
 
   function handleRadio() {
     console.log(checked);
   }
 
+  function handleCheck(id) {
+    setChecked(pre => {
+      const isChecked = checked.includes(id);
+      if(isChecked) {
+        return checked.filter(check => check != id);
+      }
+      else {
+        return [...pre,id]
+      }
+    })
+  }
+  console.log(checked);
+
   return (
     <div className="App">
 
       {/* Two-ay binding for input  */}
-      <input 
+      <input
         value={name}
         onChange={(e) => setName(e.target.value)}
       />
-      <h1>{ name }</h1> 
+      <h1>{name}</h1>
 
-      {courses.map(course =>       
-        <div key={course.id}> 
-          <input 
-            type='radio' 
+      {/* Two-way binding for radio */}
+      {/* {courses.map(course =>
+        <div key={course.id}>
+          <input
+            type='radio'
             checked={course.id === checked}
-            onChange={(e) => setChecked(course.id)} 
-          /> { course.name }
-        </div>  
+            onChange={(e) => setChecked(course.id)}
+          /> {course.name}
+        </div>
+      )} */}
+
+      {courses.map(course =>
+        <div key={course.id}>
+          <input
+            type='checkbox'
+            checked={checked.includes(course.id)}
+            onChange={() => handleCheck(course.id)}
+          /> {course.name}
+        </div>
       )}
 
-      <button onClick={handleRadio}>Sumit</button>
+      <button>Sumit</button>
     </div>
   );
 }
