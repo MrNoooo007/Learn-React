a30e5e80185ad1da50d08d352ef5079b4b722264
DatTran <khiemtran21012000@gmail.com>
2023-05-07 20:48:35 +0700

    add code for example of resize with useEffect

--- tiktok/src/Content.js
+++ tiktok/src/Content.js
@@ -1,50 +1,20 @@
 import { useEffect } from 'react';
 import { useState } from 'react';
 
-const tabs = ['posts', 'comments', 'albums']
 
 function Content() {
-    const [title, setTitle] = useState('');
-    const [posts, setPosts] = useState([]);
-    const [type, setType] = useState('posts');
-    const [showGotoTop, setGotoTop] = useState(false);
+    const [width, setWidth] = useState(window.innerWidth);
     
-    useEffect( () => {
-        fetch(`https://jsonplaceholder.typicode.com/${type}`)
-        .then(res => res.json())
-        .then(posts => {
-            setPosts(posts);
-        })
-    }, [type])
-
-    useEffect(() => {        
-        const handlScroll = () => {
-            setGotoTop(window.scrollY >= 200);
+    useEffect(() => {
+        const handlResize = () => {
+            setWidth(window.innerWidth)
         }
-
-        return () => {
-            window.removeEventListener('scroll', handlScroll);
-        }
-
-        window.addEventListener('scroll', handlScroll);
+        window.addEventListener('resize', handlResize)
     })
 
     return (
         <div>
-            {tabs.map(tab => 
-                <button 
-                    key={tab}
-                    onClick={() => setType(tab)}
-                >
-                    {tab}
-                </button>)}
-            <input 
-                value={title}
-                onChange={(e) => setTitle(e.target.value)}
-            />
-            <ul>
-                {posts.map(post => <li key={post.id}>{post.title || post.name}</li>)}
-            </ul>
+            { width }
         </div>
     )
 }
