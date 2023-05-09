0052ef050c3a76b3c9f0ea204b11c678e7257118
DatTran <khiemtran21012000@gmail.com>
2023-05-09 08:23:05 +0700

    add code for useRef

--- tiktok/src/App.js
+++ tiktok/src/App.js
@@ -1,17 +1,27 @@
 import './App.css';
-import { useState } from 'react';
+import {useRef, useState} from 'react';
 import Content from './Content';
 
 
 function App() {
-  const [show, setShow] = useState(false);
-  const handleShow = () => {
-    setShow(!show);
+  const [count, setCount] = useState(60);
+  let timerId = useRef();
+
+  const handleStartCount = () => {
+    timerId.current = setInterval(() => {
+      setCount(pre => pre + 1)
+    },1000)
   }
+
+  const handleStopCount = () => {
+    clearInterval(timerId.current)
+  }
+
   return (
     <div className="App">
-      <button onClick={handleShow}>Show / Hide</button>
-      { show && <Content/> }
+      <h1>{ count }</h1>
+      <button onClick={handleStartCount}>Start</button>
+      <button onClick={handleStopCount}>Stop</button>
     </div>
   );
 }
--- tiktok/src/Content.js
+++ tiktok/src/Content.js
@@ -3,31 +3,9 @@ import { useState } from 'react';
 
 
 function Content() {
-    const [avt, setAvt] = useState();
-
-    useEffect(() => {
-        return () => {
-            avt && URL.revokeObjectURL(avt.preview);   
-        }    
-    }, [avt])
-
-
-    const handleUploadImg = (e) => {
-        const file = e.target.files[0];
-
-        file.preview = URL.createObjectURL(file);
-        setAvt(file);
-    }
-
     return (
         <div>
-            <input 
-                type='file'
-                onChange={handleUploadImg}
-            />
-            {avt && (
-                <img src={avt.preview} />
-            )}
+            <h1>Hello</h1>
         </div>
     )
 }
