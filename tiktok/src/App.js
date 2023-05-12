69f77bd62c4af31348a13ca91fc9bb792c9eb207
DatTran <khiemtran21012000@gmail.com>
2023-05-12 14:26:12 +0700

    add code for router

--- tiktok/src/App.js
+++ tiktok/src/App.js
@@ -1,60 +1,29 @@
 import './App.css';
-import {useReducer} from 'react';
 
-// init state
-const initState = {
-  job: '',
-  jobs: []
-};
-
-// Actions
-const SET_JOB = 'set_job'
-const ADD_JOB = 'add_job'
-const DELETE_JOB = 'delete_job'
-
-const setJob = (payload) => {
-  console.log(payload);
-  return {
-    type: SET_JOB,
-    payload 
-  }
-}
-
-
-// Init Reducer
-const reducer = (state, action) => {
-  switch(action.type) {
-    case SET_JOB:
-      return {
-        ...state,
-        job: action.payload
-      }
-  }
-}
-
-
+import Home from './pages/Home'
+import Contact from './pages/Contact'
+import { Routes, Route, Link } from 'react-router-dom'
 
 function App() {
 
-  const [state, dispatch] = useReducer(reducer,initState);
-  const { jobs, job } = state;
 
   return (
     <div className="App">
-      <h3>To do</h3>
-      <input
-        value={job} 
-        placeholder='enter something'
-        onChange={(e) => {
-          dispatch(setJob(e.target.value))
-        }}
-      />
-      <button>Add</button>
-      <ul>
-      {jobs.map(job => (
-        <li key={job}>{job}</li>
-      ))}
-      </ul>
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>
+            <Link to='/contact'>Contact</Link>
+          </li>
+          <li>
+            <Link to='/home'>Home</Link>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path='/home' element={<Home/>}/>
+        <Route path='/contact' element={<Contact/>}/>
+      </Routes>
     </div>
   );
 }
